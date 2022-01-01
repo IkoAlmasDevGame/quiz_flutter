86546d36360897b0a18dd912f2b2432007bfbc7e
IkoAlmasDevGame <ikoalmas27@gmail.com>
2022-01-01 15:45:19 +0700

    Update terbaru

--- lib/Model.dart
+++ lib/Model.dart
@@ -1,9 +1,15 @@
 class Quiz {
+  // Untuk Soal
   String Exercise;
+  // untuk Jawaban A
   String AnswereA;
+  // Untuk Jawaban B
   String AnswereB;
+  // Untuk Jawaban C
   String AnswereC;
+  // Untuk Jawaban D
   String AnswereD;
+  // Untuk Number Jawaban A atau B atau C atau D
   int AnswereId;
 
   Quiz(this.Exercise, this.AnswereA, this.AnswereB, this.AnswereC, this.AnswereD, this.AnswereId);
